312708b695207c322a1505f6f94cb8eb01fe8252
Zireael07 <zi@mail.com>
2018-08-13 18:45:24 +0200

    part 2, the basics of a map system

--- player.gd
+++ player.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+const DIRECTIONS = {
+	"N":    Vector2(0,-1),
+	"NE":   Vector2(1,-1),
+	"E":    Vector2(1,0),
+	"SE":   Vector2(1,1),
+	"S":    Vector2(0,1),
+	"SW":   Vector2(-1,1),
+	"W":    Vector2(-1,0),
+	"NW":   Vector2(-1,-1),
+    }
+
+
 # class member variables go here, for example:
 var direction = Vector2()
 
@@ -37,7 +49,7 @@ func update_position(pos, direction):
 	print("Pos %s, dir %s" % [pos, direction])
 	print("Grid pos, old: %s, new: %s" % [grid_pos, new_grid_pos])
 	#print(target_pos)
-	return target_pos
+	return [target_pos, new_grid_pos]
 
 
 func _input(event):
@@ -48,15 +60,33 @@ func _input(event):
 #			print(event.scancode)
 	
 	if Input.is_action_just_pressed("move_up"):
-		direction.y = -1
-	elif Input.is_action_just_pressed("move_down"):
-		direction.y = 1
-	
+		direction = DIRECTIONS.N
+		#direction.y = -1
+	if Input.is_action_just_pressed("move_up_right"):
+		direction = DIRECTIONS.NE
+	if Input.is_action_just_pressed("move_right"):
+		direction = DIRECTIONS.E
+		#direction.x = 1
+	if Input.is_action_just_pressed("move_down_right"):
+		direction = DIRECTIONS.SE
+	if Input.is_action_just_pressed("move_down"):
+		direction = DIRECTIONS.S
+		#direction.y = 1
+	if Input.is_action_just_pressed("move_down_left"):
+		direction = DIRECTIONS.SW
 	if Input.is_action_just_pressed("move_left"):
-		direction.x = -1
-	elif Input.is_action_just_pressed("move_right"):
-		direction.x = 1
+		direction = DIRECTIONS.W
+		#direction.x = -1
+	if Input.is_action_just_pressed("move_up_left"):
+		direction = DIRECTIONS.NW
 
 	if direction != Vector2():
-		target_pos = update_position(get_position(), direction)
-		set_position(target_pos+Vector2(0,-8))
+		# makes the move happen!
+		var res = update_position(get_position(), direction)
+		
+		# Check for valid cell to step to
+		if grid.is_floor(res[1]):
+			set_position(res[0]+Vector2(0,-8))
+		# Announce when we bump something
+		else:
+        	print( "Ow! You hit a wall!" )
