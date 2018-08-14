3c4888680b7f11476e5df157ffd976746fc10db0
Zireael07 <zi@mail.com>
2018-08-14 17:29:12 +0200

    part 3, basics of entity system

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends "entity.gd"
 
 const DIRECTIONS = {
 	"N":    Vector2(0,-1),
@@ -10,32 +10,6 @@ const DIRECTIONS = {
 	"W":    Vector2(-1,0),
 	"NW":   Vector2(-1,-1),
     }
-
-
-# class member variables go here, for example:
-var direction = Vector2()
-
-var target_pos = Vector2()
-
-var grid
-# The map_to_world function returns the position of the tile's top left corner in isometric space,
-# we have to offset the objects on the Y axis to center them on the tiles
-var tile_offset = Vector2(0,0)
-var tile_size = Vector2(0,0)
-
-func cartesian_to_isometric(vector):
-	return Vector2(vector.x - vector.y, (vector.x + vector.y) / 2)
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	
-	grid = get_parent().get_node("TileMap")
-	tile_size = grid.get_cell_size()
-	tile_offset = Vector2(0, tile_size.y / 2)
-	
-	#set_process(true)
-	#pass
 
 func update_position(pos, direction):
 	var grid_pos = grid.world_to_map(pos)
