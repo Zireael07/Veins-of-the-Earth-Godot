27d842f0392c18809589d14978fa81f21cf8dd1d
Zireael07 <zi@mail.com>
2018-11-27 19:02:24 +0100

    new: display NPC hp in sidebar when mousing over

--- PlayerInfo.gd
+++ PlayerInfo.gd
@@ -8,6 +8,8 @@ onready var hplabel = get_node("VBoxContainer2/HPBox/HP")
 onready var hpbar = get_node("VBoxContainer2/HPBox/ProgressBar")
 onready var coords = get_node("VBoxContainer2/VBoxContainer/Coords")
 onready var npclabel = get_node("VBoxContainer2/VBoxContainer/NPCLabel")
+onready var hplabel_npc = get_node("VBoxContainer2/VBoxContainer/HPBoxNPC/HP2")
+onready var hpbar_npc = get_node("VBoxContainer2/VBoxContainer/HPBoxNPC/ProgressBar2")
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -27,6 +29,17 @@ func hp_changed(current,full):
 
 func _on_Node2D_cell_hover(cell):
 	coords.set_text(str(cell))
-	var entities = RPG.map.get_entities_in_cell_readable(cell)
+	var entities_text = RPG.map.get_entities_in_cell_readable(cell)
 	
-	npclabel.set_text(str(entities))
+	npclabel.set_text(str(entities_text))
+	
+	var entities = RPG.map.get_entities_in_cell(cell)
+	# if we have an entity that is an actor, display hp
+	if entities.size() > 0 and entities[0].fighter != null:
+		$"VBoxContainer2/VBoxContainer/HPBoxNPC".show()
+		hplabel_npc.set_text("HP: " + str(entities[0].fighter.max_hp) + " / "+str(entities[0].fighter.hp))
+		hpbar_npc.set_max(entities[0].fighter.max_hp)
+		hpbar_npc.set_value(entities[0].fighter.hp)
+	else:
+		$"VBoxContainer2/VBoxContainer/HPBoxNPC".hide()
+	
