ffcca9a558610b2715fc7149a660d955651de048
Zireael07 <zi@mail.com>
2019-09-12 17:32:21 +0200

    new: missed a file

---
+++ DebugMenuPanel.gd
@@ -0,0 +1,22 @@
+extends Panel
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_RevealButton_pressed():
+	
+	hide()
+	
+	RPG.map.debug_reveal()
+	
+	#pass # Replace with function body.
